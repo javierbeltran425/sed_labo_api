b5156153958ee608f5a2aeec9f9a71f8ee9f714b
javierbeltran425 <00019315@uca.edu.sv>
2021-11-19 11:51:21 -0600

    Moongo conection

--- app.js
+++ app.js
@@ -3,15 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var cors = require('cors');
+var mongoose = require('mongoose')
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,8 +14,24 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+mongoose.connect("mongodb://FoodAdmin:!rCmKkn9@localhost:27017/dbFood?authSource=admin", {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+})
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch((err) => {
+    console.log(err)
+    debug(err);
+    process.exit(1);
+  })
+
+var foodRoutes = require('./routes/FoodRouters')
+
+app.use('/food',foodRoutes)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
