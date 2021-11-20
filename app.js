85dd1fa7f4e0cc6f441d028b2f1fff5911e0679a
javierbeltran425 <00019315@uca.edu.sv>
2021-11-20 01:05:20 -0600

    api correction

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const bodyParser = require('bodyy-parser');
 var cors = require('cors');
 var mongoose = require('mongoose')
 
@@ -11,6 +12,8 @@ var app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(cors());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
